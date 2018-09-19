7257577bc39d693d815f07bedb15bb52c31dc738
午休 <shanshan.xue@hipac.cn>
2018-09-19 18:20:39 +0800

    添加 git hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,24 @@
 module.exports = {
-    parser: 'babel-eslint',
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"]
+  }
 };
--- test.js
+++ test.js
@@ -1,58 +1,54 @@
-function findFrom(formKey){
-	const { props, _form } = this;
+function findFrom(formKey) {
+    const { props, _form } = this;
 
-	if (this[formKey]){
-		return this[formKey];
-	}
-	if(props.form){
-		return props.form;
-	}
+    if (this[formKey]) {
+        return this[formKey];
+    }
+    if (props.form) {
+        return props.form;
+    }
 
-	if (_form){
-		return _form;
-	}
+    if (_form) {
+        return _form;
+    }
 }
 
-function aaa (b){
-	this.a = b;
+function aaa(b) {
+    this.a = b;
 }
 
 // 校验 form 表单提交
 @aaa('123')
 class MyTestableClass {}
 
-
 new MyTestableClass();
 
-function Validate(formKey, errorCbKey){
-	return function (target, propertyKey, descriptor) {
+function Validate(formKey, errorCbKey) {
+    return function(target, propertyKey, descriptor) {
+        const originalMethod = descriptor.value;
 
-		const originalMethod = descriptor.value;
+        descriptor.value = function(e, ...args) {
+            const from = findFrom.call(this, formKey);
 
-		descriptor.value = function (e, ...args) {
-			const from = findFrom.call(this, formKey);
+            e.preventDefault();
 
-			e.preventDefault();
-            
-			from.validateFields((err, values)=>{
-				const cbParams = [e, err, values, ...args];
+            from.validateFields((err, values) => {
+                const cbParams = [e, err, values, ...args];
 
-				if (err){
-					const errorCb = this[errorCbKey];
+                if (err) {
+                    const errorCb = this[errorCbKey];
 
-					errorCb && errorCb.apply(this, cbParams);
-                    
-					return;
-				}
+                    errorCb && errorCb.apply(this, cbParams);
 
-				originalMethod && originalMethod.apply(this, cbParams);
-			});
-		};
+                    return;
+                }
 
-		return descriptor;
-	};
+                originalMethod && originalMethod.apply(this, cbParams);
+            });
+        };
+
+        return descriptor;
+    };
 }
 
-export {
-	Validate
-};
+export { Validate };
